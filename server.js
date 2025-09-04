e0309d4fa1e194198f63771bfc600df9c5f94923
Aadhil Negim <mitnichiter@gmail.com>
2025-09-04 13:16:25 +0530

    server v2

--- server.js
+++ server.js
@@ -9,20 +9,10 @@ const PORT = process.env.PORT || 3000;
 // --- Middleware ---
 app.use(cors());
 
-
 // Serve static files (css, js, images) from the root directory.
-// This will automatically handle requests for /style.css, /client.js, etc.
 app.use(express.static(__dirname));
 
 // --- API Route ---
-// This route is checked before the catch-all.
-
-// This is the standard way to serve all static files (css, js, images) 
-// from the root directory. It's more efficient and robust.
-
-
-// --- API Route ---
-// This route will be matched before the catch-all below.
 app.get('/api/playlist', (req, res) => {
   const playlistPath = path.join(__dirname, 'playlist.json');
   fs.readFile(playlistPath, 'utf8', (err, data) => {
@@ -40,13 +30,8 @@ app.get('/api/playlist', (req, res) => {
 });
 
 // --- SPA Catch-All Route ---
-
-// This uses a regular expression to match all other GET requests.
-// It serves the main app, and must be the LAST GET route.
-// For any other GET request, serve the index.html file.
-// This is the correct way to implement a catch-all for a single-page app.
-app.get('/*', (req, res) => {
-
+// Must be the LAST GET route
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
